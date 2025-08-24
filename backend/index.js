9fa36716b0dd7c3ef37cb4231923953553b47b0d
Amaygawande <gawande.amay92@gmail.com>
2025-08-24 20:14:26 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,22 @@ import feedbackRoutes from "./src/routes/feedback.routes.js"
 
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://event-1cs27j4bt-amay-gawandes-projects.vercel.app"
+];
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.json());
